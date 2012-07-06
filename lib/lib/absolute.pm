b1026f2ee9c93bdf98817fd3473bbf1901d7855d
Torsten Raudssus <torsten@raudssus.de>
2012-07-06 20:23:57 +0000

    Import of GETTY/lib-absolute-0.004 from CPAN.
    gitpan-cpan-distribution: lib-absolute
    gitpan-cpan-version:      0.004
    gitpan-cpan-path:         GETTY/lib-absolute-0.004.tar.gz
    gitpan-cpan-author:       GETTY
    gitpan-cpan-maturity:     released

--- lib/lib/absolute.pm
+++ lib/lib/absolute.pm
@@ -3,7 +3,7 @@ BEGIN {
   $lib::absolute::AUTHORITY = 'cpan:GETTY';
 }
 {
-  $lib::absolute::VERSION = '0.003';
+  $lib::absolute::VERSION = '0.004';
 }
 # ABSTRACT: Convert all paths in @INC to absolute paths
 
@@ -30,7 +30,17 @@ sub import {
 
 1;
 
-=encoding utf8
+
+__END__
+=pod
+
+=head1 NAME
+
+lib::absolute - Convert all paths in @INC to absolute paths
+
+=head1 VERSION
+
+version 0.004
 
 =head1 SYNOPSIS
 
@@ -43,6 +53,8 @@ sub import {
 This package converts on load all your @INC path into absolute paths, if you have "." in your path, it gets additionally
 added again (and also get added as absolute path).
 
+=encoding utf8
+
 =head1 SUPPORT
 
 IRC
@@ -53,9 +65,21 @@ Repository
 
   http://github.com/Getty/p5-lib-absolute
   Pull request and additional contributors are welcome
- 
+
 Issue Tracker
 
   http://github.com/Getty/p5-lib-absolute/issues
 
+=head1 AUTHOR
+
+Torsten Raudssus <torsten@raudss.us>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Torsten Raudssus.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
 
